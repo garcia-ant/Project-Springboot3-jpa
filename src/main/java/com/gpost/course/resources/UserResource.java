8df16d5598fc645c36ccb18596b0e58e90bd3621
garcia-ant <garcantony19@gmail.com>
2024-08-15 17:01:08 +0100

    User entity and resource

---
+++ src/main/java/com/gpost/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.gpost.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gpost.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Piota", "piota@gmail.com", "939317733", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+
+}
