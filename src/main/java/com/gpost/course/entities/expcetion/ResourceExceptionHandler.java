6fd84a32238e9fa881f9598143a0f71ed85e1136
garcia-ant <garcantony19@gmail.com>
2024-08-19 00:28:58 +0100

    Exception handling -delete

--- src/main/java/com/gpost/course/entities/expcetion/ResourceExceptionHandler.java
+++ src/main/java/com/gpost/course/entities/expcetion/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.gpost.course.services.exceptions.DatabaseException;
 import com.gpost.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,9 +20,20 @@ public class ResourceExceptionHandler {
 
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandarError error2 = new StandarError(Instant.now(), status.value(), error, e.getMessage(),
+		StandarError err = new StandarError(Instant.now(), status.value(), error, e.getMessage(),
 				request.getRequestURI());
-		return ResponseEntity.status(status).body(error2);
+		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandarError> database(DatabaseException e, HttpServletRequest request) {
+
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandarError err = new StandarError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
 
 }
--- src/main/java/com/gpost/course/services/UserService.java
+++ src/main/java/com/gpost/course/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gpost.course.entities.User;
 import com.gpost.course.repositories.UserRepository;
+import com.gpost.course.services.exceptions.DatabaseException;
 import com.gpost.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -31,13 +36,26 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+	    try {
+	        repository.findById(id)
+	                .orElseThrow(() -> new ResourceNotFoundException(id));
+	        repository.deleteById(id);
+	    } catch (EmptyResultDataAccessException e) {
+	        throw new ResourceNotFoundException(id);
+	    }catch (DataIntegrityViolationException e) {
+			 throw new DatabaseException(e.getMessage());
+		}
 	}
 
+
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateDate(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateDate(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateDate(User entity, User obj) {
